a09955776160afc27f9050a10104530f53dfe0ee
realkadirovv <yakupkadyrov5@gmail.com>
2026-04-29 13:32:53 +0500

    Update pip install command to include override flag

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
-# Install dependencies
-pip install -r requirements.txt
+# Install dependencies with the override flag
+pip install -r requirements.txt --break-system-packages
 
 # Collect static files
 python3.12 manage.py collectstatic --noinput
